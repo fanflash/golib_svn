226709485a507d584f318d9df8c5d851d42dd6eb
fangao <fanflash@msn.com>
2020-04-17 10:33:00 +0800

    加入得到路径版本号的方法。

--- utils.go
+++ utils.go
@@ -5,17 +5,26 @@ import (
 )
 
 //一个目录或文件的svn地址是否跟url相同
-func IsSvnPath(target string, svnUrl string,option SvnGlobalOptions)(curUrl string, isSame bool){
-	result,err := Info(option,target)
-	if err != nil{
-		return "",false;
+func IsSvnPath(target string, svnUrl string, option SvnGlobalOptions) (curUrl string, isSame bool) {
+	result, err := Info(option, target)
+	if err != nil {
+		return "", false
 	}
-	if len(result.Entrys)<1{
-		return  "",false;
+	if len(result.Entrys) < 1 {
+		return "", false
 	}
-	curUrl,_ = url.QueryUnescape(result.Entrys[0].Url)
-	if curUrl != svnUrl{
-		svnUrl,_ = url.QueryUnescape(svnUrl)
+	curUrl, _ = url.QueryUnescape(result.Entrys[0].Url)
+	if curUrl != svnUrl {
+		svnUrl, _ = url.QueryUnescape(svnUrl)
 	}
-	return curUrl, curUrl == svnUrl;
+	return curUrl, curUrl == svnUrl
+}
+
+//得到一个路径的版本号
+func GetRevisionByPath(p string, option SvnGlobalOptions) (int, error) {
+	info, err := InfoByOne(p, option)
+	if err != nil {
+		return -1, err
+	}
+	return int(info.Entrys[0].Commit.Revision), nil
 }
